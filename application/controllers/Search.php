94f3c53b80ec44cb4ba725f05a834e89a7f057ec
Ricardo <limaricardop@gmail.com>
2022-10-04 10:28:36 -0300

    first commit

---
+++ application/controllers/Search.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Search extends CI_Controller {
+	public function index()
+	{
+		$this->load->view('pages/search');
+	}
+}
